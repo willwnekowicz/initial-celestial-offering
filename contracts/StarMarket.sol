dab2de45210ffdb28fc1f2d43b7a8b39c3dbfaaf
William Wnekowicz <william.wnekowicz@gmail.com>
2017-10-14 15:50:08 -0700

    Punk to star variable name refactoring

--- contracts/StarMarket.sol
+++ contracts/StarMarket.sol
@@ -12,20 +12,20 @@ contract StarMarket {
     uint8 public decimals;
     uint256 public totalSupply;
 
-    uint public nextPunkIndexToAssign = 0;
+    uint public nextStarIndexToAssign = 0;
 
-    bool public allPunksAssigned = false;
-    uint public punksRemainingToAssign = 0;
+    bool public allStarsAssigned = false;
+    uint public starsRemainingToAssign = 0;
 
-//mapping (address => uint) public addressToPunkIndex;
-    mapping (uint => address) public punkIndexToAddress;
+//mapping (address => uint) public addressToStarIndex;
+    mapping (uint => address) public starIndexToAddress;
 
 /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
 
     struct Offer {
     bool isForSale;
-    uint punkIndex;
+    uint starIndex;
     address seller;
     uint minValue;          // in ether
     address onlySellTo;     // specify to sell only to a specific person
@@ -33,52 +33,52 @@ contract StarMarket {
 
     struct Bid {
     bool hasBid;
-    uint punkIndex;
+    uint starIndex;
     address bidder;
     uint value;
     }
 
-// A record of punks that are offered for sale at a specific minimum value, and perhaps to a specific person
-    mapping (uint => Offer) public punksOfferedForSale;
+// A record of stars that are offered for sale at a specific minimum value, and perhaps to a specific person
+    mapping (uint => Offer) public starsOfferedForSale;
 
-// A record of the highest punk bid
-    mapping (uint => Bid) public punkBids;
+// A record of the highest star bid
+    mapping (uint => Bid) public starBids;
 
     mapping (address => uint) public pendingWithdrawals;
 
-    event Assign(address indexed to, uint256 punkIndex);
+    event Assign(address indexed to, uint256 starIndex);
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event PunkTransfer(address indexed from, address indexed to, uint256 punkIndex);
-    event PunkOffered(uint indexed punkIndex, uint minValue, address indexed toAddress);
-    event PunkBidEntered(uint indexed punkIndex, uint value, address indexed fromAddress);
-    event PunkBidWithdrawn(uint indexed punkIndex, uint value, address indexed fromAddress);
-    event PunkBought(uint indexed punkIndex, uint value, address indexed fromAddress, address indexed toAddress);
-    event PunkNoLongerForSale(uint indexed punkIndex);
+    event StarTransfer(address indexed from, address indexed to, uint256 starIndex);
+    event StarOffered(uint indexed starIndex, uint minValue, address indexed toAddress);
+    event StarBidEntered(uint indexed starIndex, uint value, address indexed fromAddress);
+    event StarBidWithdrawn(uint indexed starIndex, uint value, address indexed fromAddress);
+    event StarBought(uint indexed starIndex, uint value, address indexed fromAddress, address indexed toAddress);
+    event StarNoLongerForSale(uint indexed starIndex);
 
 /* Initializes contract with initial supply tokens to the creator of the contract */
     function StarMarket() payable {
     //        balanceOf[msg.sender] = initialSupply;        // Give the creator all initial tokens
         owner = msg.sender;
         totalSupply = 10000;                               // Update total supply
-        punksRemainingToAssign = totalSupply;
+        starsRemainingToAssign = totalSupply;
         name = "STARS";                                     // Set the name for display purposes
         symbol = "★";                                       // Set the symbol for display purposes
         decimals = 0;                                       // Amount of decimals for display purposes
     }
 
-    function setInitialOwner(address to, uint punkIndex) {
+    function setInitialOwner(address to, uint starIndex) {
         if (msg.sender != owner) throw;
-        if (allPunksAssigned) throw;
-        if (punkIndex >= 10000) throw;
-        if (punkIndexToAddress[punkIndex] != to) {
-            if (punkIndexToAddress[punkIndex] != 0x0) {
-                balanceOf[punkIndexToAddress[punkIndex]]--;
+        if (allStarsAssigned) throw;
+        if (starIndex >= 10000) throw;
+        if (starIndexToAddress[starIndex] != to) {
+            if (starIndexToAddress[starIndex] != 0x0) {
+                balanceOf[starIndexToAddress[starIndex]]--;
             } else {
-                punksRemainingToAssign--;
+                starsRemainingToAssign--;
             }
-            punkIndexToAddress[punkIndex] = to;
+            starIndexToAddress[starIndex] = to;
             balanceOf[to]++;
-            Assign(to, punkIndex);
+            Assign(to, starIndex);
         }
     }
 
@@ -92,99 +92,99 @@ contract StarMarket {
 
     function allInitialOwnersAssigned() {
         if (msg.sender != owner) throw;
-        allPunksAssigned = true;
+        allStarsAssigned = true;
     }
 
-    function getPunk(uint punkIndex) {
-        if (!allPunksAssigned) throw;
-        if (punksRemainingToAssign == 0) throw;
-        if (punkIndexToAddress[punkIndex] != 0x0) throw;
-        if (punkIndex >= 10000) throw;
-        punkIndexToAddress[punkIndex] = msg.sender;
+    function getStar(uint starIndex) {
+        if (!allStarsAssigned) throw;
+        if (starsRemainingToAssign == 0) throw;
+        if (starIndexToAddress[starIndex] != 0x0) throw;
+        if (starIndex >= 10000) throw;
+        starIndexToAddress[starIndex] = msg.sender;
         balanceOf[msg.sender]++;
-        punksRemainingToAssign--;
-        Assign(msg.sender, punkIndex);
+        starsRemainingToAssign--;
+        Assign(msg.sender, starIndex);
     }
 
-// Transfer ownership of a punk to another user without requiring payment
-    function transferPunk(address to, uint punkIndex) {
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] != msg.sender) throw;
-        if (punkIndex >= 10000) throw;
-        if (punksOfferedForSale[punkIndex].isForSale) {
-            punkNoLongerForSale(punkIndex);
+// Transfer ownership of a star to another user without requiring payment
+    function transferStar(address to, uint starIndex) {
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] != msg.sender) throw;
+        if (starIndex >= 10000) throw;
+        if (starsOfferedForSale[starIndex].isForSale) {
+            starNoLongerForSale(starIndex);
         }
-        punkIndexToAddress[punkIndex] = to;
+        starIndexToAddress[starIndex] = to;
         balanceOf[msg.sender]--;
         balanceOf[to]++;
         Transfer(msg.sender, to, 1);
-        PunkTransfer(msg.sender, to, punkIndex);
+        StarTransfer(msg.sender, to, starIndex);
     // Check for the case where there is a bid from the new owner and refund it.
     // Any other bid can stay in place.
-        Bid bid = punkBids[punkIndex];
+        Bid bid = starBids[starIndex];
         if (bid.bidder == to) {
         // Kill bid and refund value
             pendingWithdrawals[to] += bid.value;
-            punkBids[punkIndex] = Bid(false, punkIndex, 0x0, 0);
+            starBids[starIndex] = Bid(false, starIndex, 0x0, 0);
         }
     }
 
-    function punkNoLongerForSale(uint punkIndex) {
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] != msg.sender) throw;
-        if (punkIndex >= 10000) throw;
-        punksOfferedForSale[punkIndex] = Offer(false, punkIndex, msg.sender, 0, 0x0);
-        PunkNoLongerForSale(punkIndex);
+    function starNoLongerForSale(uint starIndex) {
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] != msg.sender) throw;
+        if (starIndex >= 10000) throw;
+        starsOfferedForSale[starIndex] = Offer(false, starIndex, msg.sender, 0, 0x0);
+        StarNoLongerForSale(starIndex);
     }
 
-    function offerPunkForSale(uint punkIndex, uint minSalePriceInWei) {
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] != msg.sender) throw;
-        if (punkIndex >= 10000) throw;
-        punksOfferedForSale[punkIndex] = Offer(true, punkIndex, msg.sender, minSalePriceInWei, 0x0);
-        PunkOffered(punkIndex, minSalePriceInWei, 0x0);
+    function offerStarForSale(uint starIndex, uint minSalePriceInWei) {
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] != msg.sender) throw;
+        if (starIndex >= 10000) throw;
+        starsOfferedForSale[starIndex] = Offer(true, starIndex, msg.sender, minSalePriceInWei, 0x0);
+        StarOffered(starIndex, minSalePriceInWei, 0x0);
     }
 
-    function offerPunkForSaleToAddress(uint punkIndex, uint minSalePriceInWei, address toAddress) {
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] != msg.sender) throw;
-        if (punkIndex >= 10000) throw;
-        punksOfferedForSale[punkIndex] = Offer(true, punkIndex, msg.sender, minSalePriceInWei, toAddress);
-        PunkOffered(punkIndex, minSalePriceInWei, toAddress);
+    function offerStarForSaleToAddress(uint starIndex, uint minSalePriceInWei, address toAddress) {
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] != msg.sender) throw;
+        if (starIndex >= 10000) throw;
+        starsOfferedForSale[starIndex] = Offer(true, starIndex, msg.sender, minSalePriceInWei, toAddress);
+        StarOffered(starIndex, minSalePriceInWei, toAddress);
     }
 
-    function buyPunk(uint punkIndex) payable {
-        if (!allPunksAssigned) throw;
-        Offer offer = punksOfferedForSale[punkIndex];
-        if (punkIndex >= 10000) throw;
-        if (!offer.isForSale) throw;                // punk not actually for sale
-        if (offer.onlySellTo != 0x0 && offer.onlySellTo != msg.sender) throw;  // punk not supposed to be sold to this user
+    function buyStar(uint starIndex) payable {
+        if (!allStarsAssigned) throw;
+        Offer offer = starsOfferedForSale[starIndex];
+        if (starIndex >= 10000) throw;
+        if (!offer.isForSale) throw;                // star not actually for sale
+        if (offer.onlySellTo != 0x0 && offer.onlySellTo != msg.sender) throw;  // star not supposed to be sold to this user
         if (msg.value < offer.minValue) throw;      // Didn't send enough ETH
-        if (offer.seller != punkIndexToAddress[punkIndex]) throw; // Seller no longer owner of punk
+        if (offer.seller != starIndexToAddress[starIndex]) throw; // Seller no longer owner of star
 
         address seller = offer.seller;
 
-        punkIndexToAddress[punkIndex] = msg.sender;
+        starIndexToAddress[starIndex] = msg.sender;
         balanceOf[seller]--;
         balanceOf[msg.sender]++;
         Transfer(seller, msg.sender, 1);
 
-        punkNoLongerForSale(punkIndex);
+        starNoLongerForSale(starIndex);
         pendingWithdrawals[seller] += msg.value;
-        PunkBought(punkIndex, msg.value, seller, msg.sender);
+        StarBought(starIndex, msg.value, seller, msg.sender);
 
     // Check for the case where there is a bid from the new owner and refund it.
     // Any other bid can stay in place.
-        Bid bid = punkBids[punkIndex];
+        Bid bid = starBids[starIndex];
         if (bid.bidder == msg.sender) {
         // Kill bid and refund value
             pendingWithdrawals[msg.sender] += bid.value;
-            punkBids[punkIndex] = Bid(false, punkIndex, 0x0, 0);
+            starBids[starIndex] = Bid(false, starIndex, 0x0, 0);
         }
     }
 
     function withdraw() {
-        if (!allPunksAssigned) throw;
+        if (!allStarsAssigned) throw;
         uint amount = pendingWithdrawals[msg.sender];
     // Remember to zero the pending refund before
     // sending to prevent re-entrancy attacks
@@ -192,53 +192,53 @@ contract StarMarket {
         msg.sender.transfer(amount);
     }
 
-    function enterBidForPunk(uint punkIndex) payable {
-        if (punkIndex >= 10000) throw;
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] == 0x0) throw;
-        if (punkIndexToAddress[punkIndex] == msg.sender) throw;
+    function enterBidForStar(uint starIndex) payable {
+        if (starIndex >= 10000) throw;
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] == 0x0) throw;
+        if (starIndexToAddress[starIndex] == msg.sender) throw;
         if (msg.value == 0) throw;
-        Bid existing = punkBids[punkIndex];
+        Bid existing = starBids[starIndex];
         if (msg.value <= existing.value) throw;
         if (existing.value > 0) {
         // Refund the failing bid
             pendingWithdrawals[existing.bidder] += existing.value;
         }
-        punkBids[punkIndex] = Bid(true, punkIndex, msg.sender, msg.value);
-        PunkBidEntered(punkIndex, msg.value, msg.sender);
+        starBids[starIndex] = Bid(true, starIndex, msg.sender, msg.value);
+        StarBidEntered(starIndex, msg.value, msg.sender);
     }
 
-    function acceptBidForPunk(uint punkIndex, uint minPrice) {
-        if (punkIndex >= 10000) throw;
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] != msg.sender) throw;
+    function acceptBidForStar(uint starIndex, uint minPrice) {
+        if (starIndex >= 10000) throw;
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] != msg.sender) throw;
         address seller = msg.sender;
-        Bid bid = punkBids[punkIndex];
+        Bid bid = starBids[starIndex];
         if (bid.value == 0) throw;
         if (bid.value < minPrice) throw;
 
-        punkIndexToAddress[punkIndex] = bid.bidder;
+        starIndexToAddress[starIndex] = bid.bidder;
         balanceOf[seller]--;
         balanceOf[bid.bidder]++;
         Transfer(seller, bid.bidder, 1);
 
-        punksOfferedForSale[punkIndex] = Offer(false, punkIndex, bid.bidder, 0, 0x0);
+        starsOfferedForSale[starIndex] = Offer(false, starIndex, bid.bidder, 0, 0x0);
         uint amount = bid.value;
-        punkBids[punkIndex] = Bid(false, punkIndex, 0x0, 0);
+        starBids[starIndex] = Bid(false, starIndex, 0x0, 0);
         pendingWithdrawals[seller] += amount;
-        PunkBought(punkIndex, bid.value, seller, bid.bidder);
+        StarBought(starIndex, bid.value, seller, bid.bidder);
     }
 
-    function withdrawBidForPunk(uint punkIndex) {
-        if (punkIndex >= 10000) throw;
-        if (!allPunksAssigned) throw;
-        if (punkIndexToAddress[punkIndex] == 0x0) throw;
-        if (punkIndexToAddress[punkIndex] == msg.sender) throw;
-        Bid bid = punkBids[punkIndex];
+    function withdrawBidForStar(uint starIndex) {
+        if (starIndex >= 10000) throw;
+        if (!allStarsAssigned) throw;
+        if (starIndexToAddress[starIndex] == 0x0) throw;
+        if (starIndexToAddress[starIndex] == msg.sender) throw;
+        Bid bid = starBids[starIndex];
         if (bid.bidder != msg.sender) throw;
-        PunkBidWithdrawn(punkIndex, bid.value, msg.sender);
+        StarBidWithdrawn(starIndex, bid.value, msg.sender);
         uint amount = bid.value;
-        punkBids[punkIndex] = Bid(false, punkIndex, 0x0, 0);
+        starBids[starIndex] = Bid(false, starIndex, 0x0, 0);
     // Refund the bid money
         msg.sender.transfer(amount);
     }
