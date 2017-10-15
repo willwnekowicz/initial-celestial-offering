37abcd076a8c63b39af16e83ad8efbbb843110f3
William Wnekowicz <william.wnekowicz@gmail.com>
2017-10-15 12:07:37 -0700

    Update config to include staging and live networks

--- truffle.js
+++ truffle.js
@@ -5,6 +5,18 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: 3,
+      from: "0xDC2c7AEbd98569629fe249522080d6F5f2CCEE7E",
+    },
+    live: {
+      host: "localhost",
+      port: 8545,
+      network_id: 1,
+      from: "0xDC2c7AEbd98569629fe249522080d6F5f2CCEE7E",
     }
   }
 };
